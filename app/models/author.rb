ebb9ef03d8f15cd0ffd965c911a39c1f5512adf5
krissasantos <krissa.santos31@gmail.com>
2016-10-26 12:56:27 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true#, message: "No blankspaces please." 
+  validates :email, uniqueness: true#, message: "Already taken."
+  validates :phone_number, length: { is: 10 }#, message: "Should have 10 digits."
 end
